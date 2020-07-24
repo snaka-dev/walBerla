12bd95b7923493938fd162947ecb168612f071c6
markus holzer <markus.holzer@fau.de>
2020-07-24 15:59:43 +0200

    First version of setup.py file

---
+++ python/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+# install package localy with 'pip install -e .'
+
+from setuptools import setup
+setup(name='pystencils_walberla',
+      version='1.0',
+      description='Python packages for WaLBerla kernel generation',
+      keywords='walberla pystencils lbmpy',
+      author='LSS',
+      author_email='cs10-contact@fau.de',
+      url='https://walberla.net/',
+      packages=['pystencils_walberla', 'lbmpy_walberla'],
+      install_requires=['pystencils', 'lbmpy', 'sympy>=1.1', 'numpy', 'jinja2'],
+      package_data={
+          # Include all template files
+          "": ['.tmpl.*'],
+          # Or specifics files:
+          "hello": ["*.msg"],
+      },
+      extras_require={
+          'cpuinfo': ['py-cpuinfo'],
+      },
+      python_requires='>=3.6',
+      )
